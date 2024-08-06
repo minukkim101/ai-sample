495495cb3253218751339a31e376f8ddf51bb97b
minukkim <minukkim.101@gmail.com>
2024-08-06 21:52:05 +0900

    update

--- app.py
+++ app.py
@@ -1,60 +1,64 @@
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
 from langchain.chat_models.bedrock import BedrockChat
 # from langchain.chat_models.anthropic import ChatAnthropic
 import streamlit as st
-import os
+from langchain_aws import ChatBedrock
 
 # Reference: https://python.langchain.com/docs/integrations/memory/streamlit_chat_message_history
-st.title("📖 StreamlitChatMessageHistory")
+st.title("MessageHistory")
 
-model_id = 'anthropic.claude-v2'
-llm = BedrockChat(
-    model_id=model_id,
+llm = ChatBedrock(
+    model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+    region_name="us-east-1",
+    model_kwargs=dict(temperature=0),
     streaming=True
+    # other params...
 )
 
-msgs = StreamlitChatMessageHistory(key='langchain_messages')
-if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
+history = StreamlitChatMessageHistory(key='chat_messages')
+if len(history.messages) == 0:
+    history.add_ai_message("How can I help you?")
 
 view_messages = st.expander("View the message contents in session state")
 
+system_prompt = """You are an AI chatbot having a conversation with a human.
+You answer always in korean.
+You must not answer questions on investment with the aim of making a profit.
+"""
+
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are an AI chatbot having a conversation with a human."),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{question}"),
+        ("system", system_prompt),
+        (
+            "human",
+            """
+            Current conversation:
+            <conversation_history>
+            {history}
+            </conversation_history>
+
+            Here is the human's next reply:
+            <human_reply>
+            {human_input}
+            </human_reply>
+            """,
+        ),
     ]
 )
 
-chain = prompt | llm
-chain_with_history = RunnableWithMessageHistory(
-    chain,
-    lambda session_id: msgs,
-    input_messages_key="question",
-    history_messages_key="history",
-)
+chain = prompt | llm | StrOutputParser()
 
-for msg in msgs.messages:
+
+for msg in history.messages:
     st.chat_message(msg.type).write(msg.content)
 
-if prompt := st.chat_input():
-    st.chat_message("human").write(prompt)
-    config = {"configurable": {"session_id": "special_key"}}
-    response = chain_with_history.invoke({"question": prompt}, config)
-    st.chat_message("ai").write(response.content)
+if human_input := st.chat_input(key="input"):
+    st.chat_message("human").write(human_input)
+    response = chain.invoke({"history": history, "human_input": human_input})
+    history.add_user_message(human_input)
+    history.add_ai_message(response)
+    st.chat_message("ai").write(response)
 
-
-# Draw the messages at the end, so newly generated ones show up immediately
-with view_messages:
-    """
-    Message History initialized with:
-    ```python
-    msgs = StreamlitChatMessageHistory(key="langchain_messages")
-    ```
-
-    Contents of `st.session_state.langchain_messages`:
-    """
-    view_messages.json(st.session_state.langchain_messages)
